2035b3580be693603901ab22bca6f53ab54e40df
rajasekar <gee.rajasekar@gmail.com>
2022-04-11 21:21:57 +0530

    Network layer completed

--- MovieListing/MovieListing/Home/Controller/ViewController.swift
+++ MovieListing/MovieListing/Home/Controller/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-//
+
+    var movieViewModel:MovieListviewModel = MovieListviewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        movieViewModel.callPopularMovieListAPI(with: 1) { content in
+            print(content)
+        } failureHandler: { error in
+            
+        }
+        
     }
 
 //
---
+++ MovieListing/MovieListing/Home/ViewModel/MovieListingviewModel.swift
@@ -0,0 +1,26 @@
+//
+//  MovieListingviewModel.swift
+//  MovieListing
+//
+//  Created by Rajasekaran Gopal on 11/04/22.
+//
+
+import Foundation
+
+
+class MovieListviewModel {
+    
+    //MARK: - API CALLS
+    func callPopularMovieListAPI(with pageNo: Int,
+                                successHandler: @escaping (_ content: PopularMovies) -> (),
+                                failureHandler: @escaping (_ error: Error) -> ()) {
+        NetworkManager.sharedInstance.performRequest(forAPI: .loadPopularList(apiKey: User.apiKey.rawValue, lang: "en-US", pageNumber: pageNo), responseType: PopularMovies.self)
+        { (homeResponse) in
+            successHandler(homeResponse)
+        } errorHandler: { (error) in
+            //FIXME: - Show proper error screen
+            print("~~~~IN API ERROR => \(error)~~~");
+            
+        }
+    }
+}
